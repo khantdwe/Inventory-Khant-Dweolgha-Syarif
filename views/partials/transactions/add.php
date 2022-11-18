51e102883c7f8c95ae84d89618c2dbcee7bf52cc
khantdwe <118492772+khantdwe@users.noreply.github.com>
2022-11-18 09:20:24 +0700

    Add files via upload

---
+++ views/partials/transactions/add.php
@@ -0,0 +1,195 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "add-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="add"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title">Add New Transactions</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form id="transactions-add-form" role="form" novalidate enctype="multipart/form-data" class="form page-form form-horizontal needs-validation" action="<?php print_link("transactions/add?csrf_token=$csrf_token") ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="type">Type <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-type"  value="<?php  echo $this->set_field_value('type',""); ?>" type="text" placeholder="Enter Type"  required="" name="type"  class="form-control " />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="title">Title <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-title"  value="<?php  echo $this->set_field_value('title',""); ?>" type="text" placeholder="Enter Title"  required="" name="title"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="reference">Reference <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-reference"  value="<?php  echo $this->set_field_value('reference',""); ?>" type="text" placeholder="Enter Reference"  required="" name="reference"  class="form-control " />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="client_id">Client Id <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="">
+                                                            <input id="ctrl-client_id"  value="<?php  echo $this->set_field_value('client_id',""); ?>" type="number" placeholder="Enter Client Id" step="1"  required="" name="client_id"  class="form-control " />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group ">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <label class="control-label" for="sale_id">Sale Id <span class="text-danger">*</span></label>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="">
+                                                                <input id="ctrl-sale_id"  value="<?php  echo $this->set_field_value('sale_id',""); ?>" type="number" placeholder="Enter Sale Id" step="1"  required="" name="sale_id"  class="form-control " />
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group ">
+                                                        <div class="row">
+                                                            <div class="col-sm-4">
+                                                                <label class="control-label" for="provider_id">Provider Id <span class="text-danger">*</span></label>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <div class="">
+                                                                    <input id="ctrl-provider_id"  value="<?php  echo $this->set_field_value('provider_id',""); ?>" type="number" placeholder="Enter Provider Id" step="1"  required="" name="provider_id"  class="form-control " />
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group ">
+                                                            <div class="row">
+                                                                <div class="col-sm-4">
+                                                                    <label class="control-label" for="transfer_id">Transfer Id <span class="text-danger">*</span></label>
+                                                                </div>
+                                                                <div class="col-sm-8">
+                                                                    <div class="">
+                                                                        <input id="ctrl-transfer_id"  value="<?php  echo $this->set_field_value('transfer_id',""); ?>" type="number" placeholder="Enter Transfer Id" step="1"  required="" name="transfer_id"  class="form-control " />
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="form-group ">
+                                                                <div class="row">
+                                                                    <div class="col-sm-4">
+                                                                        <label class="control-label" for="payment_method_id">Payment Method Id <span class="text-danger">*</span></label>
+                                                                    </div>
+                                                                    <div class="col-sm-8">
+                                                                        <div class="">
+                                                                            <input id="ctrl-payment_method_id"  value="<?php  echo $this->set_field_value('payment_method_id',""); ?>" type="number" placeholder="Enter Payment Method Id" step="1"  required="" name="payment_method_id"  class="form-control " />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group ">
+                                                                    <div class="row">
+                                                                        <div class="col-sm-4">
+                                                                            <label class="control-label" for="amount">Amount <span class="text-danger">*</span></label>
+                                                                        </div>
+                                                                        <div class="col-sm-8">
+                                                                            <div class="">
+                                                                                <input id="ctrl-amount"  value="<?php  echo $this->set_field_value('amount',""); ?>" type="number" placeholder="Enter Amount" step="0.1"  required="" name="amount"  class="form-control " />
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group ">
+                                                                        <div class="row">
+                                                                            <div class="col-sm-4">
+                                                                                <label class="control-label" for="user_id">User Id <span class="text-danger">*</span></label>
+                                                                            </div>
+                                                                            <div class="col-sm-8">
+                                                                                <div class="">
+                                                                                    <input id="ctrl-user_id"  value="<?php  echo $this->set_field_value('user_id',""); ?>" type="number" placeholder="Enter User Id" step="1"  required="" name="user_id"  class="form-control " />
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="form-group ">
+                                                                            <div class="row">
+                                                                                <div class="col-sm-4">
+                                                                                    <label class="control-label" for="created_at">Created At <span class="text-danger">*</span></label>
+                                                                                </div>
+                                                                                <div class="col-sm-8">
+                                                                                    <div class="">
+                                                                                        <input id="ctrl-created_at"  value="<?php  echo $this->set_field_value('created_at',""); ?>" type="text" placeholder="Enter Created At"  required="" name="created_at"  class="form-control " />
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group ">
+                                                                                <div class="row">
+                                                                                    <div class="col-sm-4">
+                                                                                        <label class="control-label" for="updated_at">Updated At <span class="text-danger">*</span></label>
+                                                                                    </div>
+                                                                                    <div class="col-sm-8">
+                                                                                        <div class="input-group">
+                                                                                            <input id="ctrl-updated_at" class="form-control datepicker  datepicker" required="" value="<?php  echo $this->set_field_value('updated_at',""); ?>" type="datetime"  name="updated_at" placeholder="Enter Updated At" data-enable-time="true" data-min-date="" data-max-date="" data-date-format="Y-m-d H:i:S" data-alt-format="F j, Y - H:i" data-inline="false" data-no-calendar="false" data-mode="single" /> 
+                                                                                                <div class="input-group-append">
+                                                                                                    <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group form-submit-btn-holder text-center mt-3">
+                                                                                <div class="form-ajax-status"></div>
+                                                                                <button class="btn btn-primary" type="submit">
+                                                                                    Submit
+                                                                                    <i class="fa fa-send"></i>
+                                                                                </button>
+                                                                            </div>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </section>
