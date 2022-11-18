6d57de650557fb6634d32c921afb6ee023510a24
khantdwe <118492772+khantdwe@users.noreply.github.com>
2022-11-18 09:13:19 +0700

    Add files via upload

---
+++ views/partials/clients/edit.php
@@ -0,0 +1,198 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "edit-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$data = $this->view_data;
+//$rec_id = $data['__tableprimarykey'];
+$page_id = $this->route->page_id;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="edit"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title">Edit  Clients</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form novalidate  id="" role="form" enctype="multipart/form-data"  class="form page-form form-horizontal needs-validation" action="<?php print_link("clients/edit/$page_id/?csrf_token=$csrf_token"); ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="document_type">Document Type <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-document_type"  value="<?php  echo $data['document_type']; ?>" type="text" placeholder="Enter Document Type"  required="" name="document_type"  class="form-control " />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="document_id">Document Id <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-document_id"  value="<?php  echo $data['document_id']; ?>" type="number" placeholder="Enter Document Id" step="1"  required="" name="document_id"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="name">Name <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-name"  value="<?php  echo $data['name']; ?>" type="text" placeholder="Enter Name"  required="" name="name"  class="form-control " />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="email">Email <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="">
+                                                            <input id="ctrl-email"  value="<?php  echo $data['email']; ?>" type="email" placeholder="Enter Email"  required="" name="email"  class="form-control " />
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group ">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <label class="control-label" for="phone">Phone <span class="text-danger">*</span></label>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="">
+                                                                <input id="ctrl-phone"  value="<?php  echo $data['phone']; ?>" type="text" placeholder="Enter Phone"  required="" name="phone"  class="form-control " />
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group ">
+                                                        <div class="row">
+                                                            <div class="col-sm-4">
+                                                                <label class="control-label" for="last_purchase">Last Purchase <span class="text-danger">*</span></label>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <div class="">
+                                                                    <input id="ctrl-last_purchase"  value="<?php  echo $data['last_purchase']; ?>" type="text" placeholder="Enter Last Purchase"  required="" name="last_purchase"  class="form-control " />
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group ">
+                                                            <div class="row">
+                                                                <div class="col-sm-4">
+                                                                    <label class="control-label" for="total_purchases">Total Purchases <span class="text-danger">*</span></label>
+                                                                </div>
+                                                                <div class="col-sm-8">
+                                                                    <div class="">
+                                                                        <input id="ctrl-total_purchases"  value="<?php  echo $data['total_purchases']; ?>" type="number" placeholder="Enter Total Purchases" step="1"  required="" name="total_purchases"  class="form-control " />
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="form-group ">
+                                                                <div class="row">
+                                                                    <div class="col-sm-4">
+                                                                        <label class="control-label" for="total_paid">Total Paid <span class="text-danger">*</span></label>
+                                                                    </div>
+                                                                    <div class="col-sm-8">
+                                                                        <div class="">
+                                                                            <input id="ctrl-total_paid"  value="<?php  echo $data['total_paid']; ?>" type="number" placeholder="Enter Total Paid" step="0.1"  required="" name="total_paid"  class="form-control " />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="form-group ">
+                                                                    <div class="row">
+                                                                        <div class="col-sm-4">
+                                                                            <label class="control-label" for="created_at">Created At <span class="text-danger">*</span></label>
+                                                                        </div>
+                                                                        <div class="col-sm-8">
+                                                                            <div class="">
+                                                                                <input id="ctrl-created_at"  value="<?php  echo $data['created_at']; ?>" type="text" placeholder="Enter Created At"  required="" name="created_at"  class="form-control " />
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="form-group ">
+                                                                        <div class="row">
+                                                                            <div class="col-sm-4">
+                                                                                <label class="control-label" for="updated_at">Updated At <span class="text-danger">*</span></label>
+                                                                            </div>
+                                                                            <div class="col-sm-8">
+                                                                                <div class="input-group">
+                                                                                    <input id="ctrl-updated_at" class="form-control datepicker  datepicker" required="" value="<?php  echo $data['updated_at']; ?>" type="datetime"  name="updated_at" placeholder="Enter Updated At" data-enable-time="true" data-min-date="" data-max-date="" data-date-format="Y-m-d H:i:S" data-alt-format="F j, Y - H:i" data-inline="false" data-no-calendar="false" data-mode="single" /> 
+                                                                                        <div class="input-group-append">
+                                                                                            <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="form-group ">
+                                                                            <div class="row">
+                                                                                <div class="col-sm-4">
+                                                                                    <label class="control-label" for="deleted_at">Deleted At <span class="text-danger">*</span></label>
+                                                                                </div>
+                                                                                <div class="col-sm-8">
+                                                                                    <div class="">
+                                                                                        <input id="ctrl-deleted_at"  value="<?php  echo $data['deleted_at']; ?>" type="text" placeholder="Enter Deleted At"  required="" name="deleted_at"  class="form-control " />
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-group ">
+                                                                                <div class="row">
+                                                                                    <div class="col-sm-4">
+                                                                                        <label class="control-label" for="balance">Balance <span class="text-danger">*</span></label>
+                                                                                    </div>
+                                                                                    <div class="col-sm-8">
+                                                                                        <div class="">
+                                                                                            <input id="ctrl-balance"  value="<?php  echo $data['balance']; ?>" type="number" placeholder="Enter Balance" step="0.1"  required="" name="balance"  class="form-control " />
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="form-ajax-status"></div>
+                                                                            <div class="form-group text-center">
+                                                                                <button class="btn btn-primary" type="submit">
+                                                                                    Update
+                                                                                    <i class="fa fa-send"></i>
+                                                                                </button>
+                                                                            </div>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </section>
