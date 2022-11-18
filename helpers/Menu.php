046bccdaf3ea73a8f3bdc2cb889d89d4e7e03abb
khantdwe <118492772+khantdwe@users.noreply.github.com>
2022-11-18 09:21:33 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'clients', 
+			'label' => 'Clients', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'payment_methods', 
+			'label' => 'Payment Methods', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'product_categories', 
+			'label' => 'Product Categories', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'products', 
+			'label' => 'Products', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'providers', 
+			'label' => 'Providers', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'receipts', 
+			'label' => 'Receipts', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'received_products', 
+			'label' => 'Received Products', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'sales', 
+			'label' => 'Sales', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'sold_products', 
+			'label' => 'Sold Products', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transactions', 
+			'label' => 'Transactions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'transfers', 
+			'label' => 'Transfers', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
